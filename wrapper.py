08f50eefc967bf01efa38d8b09c93437a8df0dae
RonSchlichting <76098278+RonSchlichting@users.noreply.github.com>
2022-02-07 14:40:09 +0100

    added flask_cors to library and allowing CORS for all domains

--- wrapper.py
+++ wrapper.py
@@ -1,16 +1,19 @@
 import os
 
 from flask import Flask, request
-
+from flask_cors import CORS
 from json_to_graphviz_svg import convert_json_to_graphviz_svg
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/", methods = ['POST'])
+
+@app.route("/", methods=['POST'])
 def default():
     body = request.json
     svg = convert_json_to_graphviz_svg(body)
     return svg
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
